4e5d49547b0361c146c24a7b85228f794ad5c902
mottakinrahat <rahatchowdhury661@gmail.com>
2025-09-18 18:11:43 +0600

    dd

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,100 +1,74 @@
+
+import { sendResponse } from "../../../helpers/sendResponse";
+import { catchAsync } from "../../../helpers/trycatch";
 import { pick } from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import { AdminServices } from "./admin.services";
 
-const getAllAdminFromDB = async (req: any, res: any) => {
-  try {
-    const filter = pick(req.query, adminFilterableFields);
-    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
-    const result = await AdminServices.getAllAdmin(filter, options);
-    res.status(200).json({
-      success: true,
-      message: "Admin data retrieved successfully",
-      meta: result?.meta,
-      data: result?.data,
-    });
-  } catch (error) {
-    console.error("Error retrieving admin data:", error);
-    res.status(500).json({
-      success: false,
-      message: "Failed to retrieve admin data",
-      error: error || "Internal Server Error",
-    });
-  }
-};
-const getSingleAdmin = async (req: any, res: any) => {
-  try {
-    const { id } = req.params;
-    const result = await AdminServices.getSingleAdminFromDB(id);
-    res.status(200).json({
-      success: true,
-      message: "Single data retrieved successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.error("Error retrieving admin data:", error);
-    res.status(500).json({
-      success: false,
-      message: "Failed to Single retrieve admin data",
-      error: error || "Internal Server Error",
-    });
-  }
-};
-const updateAdminData = async (req: any, res: any) => {
-  try {
-    const { id } = req.params;
-    const result = await AdminServices.updateAdminDataFromDB(id, req.body);
-    res.status(200).json({
-      success: true,
-      message: "admin data updated successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.error("Error retrieving admin data:", error);
-    res.status(500).json({
-      success: false,
-      message: "failed to update admin data",
-      error: error || "Internal Server Error",
-    });
-  }
-};
-const deleteAdminData = async (req: any, res: any) => {
-  try {
-    const { id } = req.params;
-    const result = await AdminServices.deleteAdminFromDB(id);
-    res.status(200).json({
-      success: true,
-      message: "admin data deleted successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.error("Error deleting admin data:", error);
-    res.status(500).json({
-      success: false,
-      message: "failed to delete admin data",
-      error: error || "Internal Server Error",
-    });
-  }
-};
-const softDeleteAdminData = async (req: any, res: any) => {
-  try {
-    const { id } = req.params;
-    console.log(id)
-    const result = await AdminServices.softDeleteAdminFromDB(id);
-    res.status(200).json({
-      success: true,
-      message: "admin data softly deleted successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.error("Error deleting admin softly data:", error);
-    res.status(500).json({
-      success: false,
-      message: "failed to delete admin data",
-      error: error || "Internal Server Error",
-    });
-  }
-};
+
+const getAllAdminFromDB = catchAsync(async (req, res, next) => {
+  const filter = pick(req.query, adminFilterableFields);
+  const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+  const result = await AdminServices.getAllAdmin(filter, options);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Admin data retrieved successfully",
+    meta: result?.meta,
+    data: result?.data,
+  });
+});
+
+const getSingleAdmin = catchAsync(async (req: any, res: any) => {
+  const { id } = req.params;
+  const result = await AdminServices.getSingleAdminFromDB(id);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Single data retrieved successfully",
+    data: result,
+  });
+});
+
+const updateAdminData = catchAsync(async (req: any, res: any) => {
+  const { id } = req.params;
+  const result = await AdminServices.updateAdminDataFromDB(id, req.body);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Admin data updated successfully",
+    data: result,
+  });
+});
+
+const deleteAdminData = catchAsync(async (req: any, res: any) => {
+  const { id } = req.params;
+  const result = await AdminServices.deleteAdminFromDB(id);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Admin data deleted successfully",
+    data: result,
+  });
+});
+
+const softDeleteAdminData = catchAsync(async (req: any, res: any) => {
+  const { id } = req.params;
+  console.log(id);
+  const result = await AdminServices.softDeleteAdminFromDB(id);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Admin data softly deleted successfully",
+    data: result,
+  });
+});
+
 export const AdminController = {
   getAllAdminFromDB,
   getSingleAdmin,
--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,13 +1,33 @@
-
-import express  from 'express';
-import { AdminController } from './admin.controller';
-
+import express, { NextFunction, Request, Response } from "express";
+import { AdminController } from "./admin.controller";
+import { z, ZodObject } from "zod";
 const router = express.Router();
-
-router.get('/admins', AdminController.getAllAdminFromDB);
-router.get('/admins/:id', AdminController.getSingleAdmin);
-router.patch('/admins/:id', AdminController.updateAdminData);
-router.delete('/admins/:id', AdminController.deleteAdminData);
-router.delete('/admins/soft/:id', AdminController.softDeleteAdminData);
+const update = z.object({
+  body: z.object({
+    name: z.string(),
+    contactNumber: z.string(),
+  }),
+});
+const validateRequest =
+  (schema: ZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req?.body,
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+router.get("/admins", AdminController.getAllAdminFromDB);
+router.get("/admins/:id", AdminController.getSingleAdmin);
+router.patch(
+  "/admins/:id",
+  validateRequest(update),
+  AdminController.updateAdminData
+);
+router.delete("/admins/:id", AdminController.deleteAdminData);
+router.delete("/admins/soft/:id", AdminController.softDeleteAdminData);
 
 export const adminRoutes = router;
